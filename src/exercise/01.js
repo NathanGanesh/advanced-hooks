f092045bb220faa0d0a5be4c6b37a9fdd6bcdad2
nathan ganesh <489290@student.saxion.nl>
2021-03-08 00:26:18 +0100

    first commit

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,23 +1,28 @@
 // useReducer: simple Counter
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
+import * as React from 'react';
+const countReducer = ({ count }, change) => {
+	return change;
+};
 
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
-
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
-  return <button onClick={increment}>{count}</button>
+function Counter({ initialCount = 0, step = 1 }) {
+	// 🐨 replace React.useState with Rceat.useReducer.
+	// 💰 React.useReducer(countReducer, initialCount)
+	const [ state, setState ] = React.useReducer(countReducer, {
+		count: initialCount
+	});
+	const { count } = state;
+	const increment = () => setState({ count: count + step });
+	// 💰 you can write the countReducer function so you don't have to make any
+	// changes to the next two lines of code! Remember:
+	// The 1st argument is called "state" - the current value of count
+	// The 2nd argument is called "newState" - the value passed to setCount
+	return <button onClick={increment}>{count}</button>;
 }
 
 function App() {
-  return <Counter />
+	return <Counter />;
 }
 
-export default App
+export default App;
--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,137 +1,129 @@
 // useCallback: custom hooks
 // http://localhost:3000/isolated/exercise/02.js
 
-import * as React from 'react'
-import {
-  fetchPokemon,
-  PokemonForm,
-  PokemonDataView,
-  PokemonInfoFallback,
-  PokemonErrorBoundary,
-} from '../pokemon'
+import * as React from 'react';
 
-// 🐨 this is going to be our generic asyncReducer
-function pokemonInfoReducer(state, action) {
-  switch (action.type) {
-    case 'pending': {
-      // 🐨 replace "pokemon" with "data"
-      return {status: 'pending', pokemon: null, error: null}
-    }
-    case 'resolved': {
-      // 🐨 replace "pokemon" with "data" (in the action too!)
-      return {status: 'resolved', pokemon: action.pokemon, error: null}
-    }
-    case 'rejected': {
-      // 🐨 replace "pokemon" with "data"
-      return {status: 'rejected', pokemon: null, error: action.error}
-    }
-    default: {
-      throw new Error(`Unhandled action type: ${action.type}`)
-    }
-  }
+import { PokemonDataView, PokemonErrorBoundary, PokemonForm, PokemonInfoFallback, fetchPokemon } from '../pokemon';
+
+function asyncReducer(state, action) {
+	switch (action.type) {
+		case 'pending': {
+			// 🐨 replace "pokemon" with "data"
+			return { status: 'pending', data: null, error: null };
+		}
+		case 'resolved': {
+			// 🐨 replace "data" with "data" (in the action too!)
+			return { status: 'resolved', data: action.data, error: null };
+		}
+		case 'rejected': {
+			// 🐨 replace "data" with "data"
+			return { status: 'rejected', data: null, error: action.error };
+		}
+		default: {
+			throw new Error(`Unhandled action type: ${action.type}`);
+		}
+	}
 }
 
-function PokemonInfo({pokemonName}) {
-  // 🐨 move both the useReducer and useEffect hooks to a custom hook called useAsync
-  // here's how you use it:
-  // const state = useAsync(
-  //   () => {
-  //     if (!pokemonName) {
-  //       return
-  //     }
-  //     return fetchPokemon(pokemonName)
-  //   },
-  //   {status: pokemonName ? 'pending' : 'idle'},
-  //   [pokemonName],
-  // )
-  // 🐨 so your job is to create a useAsync function that makes this work.
-  const [state, dispatch] = React.useReducer(pokemonInfoReducer, {
-    status: pokemonName ? 'pending' : 'idle',
-    // 🐨 this will need to be "data" instead of "pokemon"
-    pokemon: null,
-    error: null,
-  })
+function useAsync(asyncCallback, initialState, dependencies) {
+	const [ state, dispatch ] = React.useReducer(asyncReducer, {
+		status: 'idle',
+		// 🐨 this will need to be "data" instead of "pokemon"
+		data: null,
+		error: null,
+		...initialState
+	});
 
-  React.useEffect(() => {
-    // 💰 this first early-exit bit is a little tricky, so let me give you a hint:
-    // const promise = asyncCallback()
-    // if (!promise) {
-    //   return
-    // }
-    // then you can dispatch and handle the promise etc...
-    if (!pokemonName) {
-      return
-    }
-    dispatch({type: 'pending'})
-    fetchPokemon(pokemonName).then(
-      pokemon => {
-        dispatch({type: 'resolved', pokemon})
-      },
-      error => {
-        dispatch({type: 'rejected', error})
-      },
-    )
-    // 🐨 you'll accept dependencies as an array and pass that here.
-    // 🐨 because of limitations with ESLint, you'll need to ignore
-    // the react-hooks/exhaustive-deps rule. We'll fix this in an extra credit.
-  }, [pokemonName])
+	React.useEffect(() => {
+		// 💰 this first early-exit bit is a little tricky, so let me give you a hint:
+		const promise = asyncCallback();
+		if (!promise) {
+			return;
+		}
+		// then you can dispatch and handle the promise etc...
+		// if (!pokemonName) {
+		// 	return;
+		// }
+		dispatch({ type: 'pending' });
+		promise.then(
+			(data) => {
+				dispatch({ type: 'resolved', data });
+			},
+			(error) => {
+				dispatch({ type: 'rejected', error });
+			}
+		);
+	}, dependencies);
+	return state;
+}
 
-  // 🐨 this will change from "pokemon" to "data"
-  const {pokemon, status, error} = state
+function PokemonInfo({ pokemonName }) {
+	// 🐨 move both the useReducer and useEffect hooks to a custom hook called useAsync
+	// here's how you use it:
+	const state = useAsync(
+		() => {
+			if (!pokemonName) {
+				return;
+			}
+			return fetchPokemon(pokemonName);
+		},
+		{ status: pokemonName ? 'pending' : 'idle' },
+		[ pokemonName ]
+	);
+	// 🐨 so your job is to create a useAsync function that makes this work.
 
-  if (status === 'idle' || !pokemonName) {
-    return 'Submit a pokemon'
-  } else if (status === 'pending') {
-    return <PokemonInfoFallback name={pokemonName} />
-  } else if (status === 'rejected') {
-    throw error
-  } else if (status === 'resolved') {
-    return <PokemonDataView pokemon={pokemon} />
-  }
+	// 🐨 this will change from "pokemon" to "data"
+	const { pokemon, status, error } = state;
 
-  throw new Error('This should be impossible')
+	if (status === 'idle' || !pokemonName) {
+		return 'Submit a pokemon';
+	} else if (status === 'pending') {
+		return <PokemonInfoFallback name={pokemonName} />;
+	} else if (status === 'rejected') {
+		throw error;
+	} else if (status === 'resolved') {
+		return <PokemonDataView pokemon={pokemon} />;
+	}
+
+	throw new Error('This should be impossible');
 }
 
 function App() {
-  const [pokemonName, setPokemonName] = React.useState('')
+	const [ pokemonName, setPokemonName ] = React.useState('');
 
-  function handleSubmit(newPokemonName) {
-    setPokemonName(newPokemonName)
-  }
+	function handleSubmit(newPokemonName) {
+		setPokemonName(newPokemonName);
+	}
 
-  function handleReset() {
-    setPokemonName('')
-  }
+	function handleReset() {
+		setPokemonName('');
+	}
 
-  return (
-    <div className="pokemon-info-app">
-      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
-      <hr />
-      <div className="pokemon-info">
-        <PokemonErrorBoundary onReset={handleReset} resetKeys={[pokemonName]}>
-          <PokemonInfo pokemonName={pokemonName} />
-        </PokemonErrorBoundary>
-      </div>
-    </div>
-  )
+	return (
+		<div className="pokemon-info-app">
+			<PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+			<hr />
+			<div className="pokemon-info">
+				<PokemonErrorBoundary onReset={handleReset} resetKeys={[ pokemonName ]}>
+					<PokemonInfo pokemonName={pokemonName} />
+				</PokemonErrorBoundary>
+			</div>
+		</div>
+	);
 }
 
 function AppWithUnmountCheckbox() {
-  const [mountApp, setMountApp] = React.useState(true)
-  return (
-    <div>
-      <label>
-        <input
-          type="checkbox"
-          checked={mountApp}
-          onChange={e => setMountApp(e.target.checked)}
-        />{' '}
-        Mount Component
-      </label>
-      <hr />
-      {mountApp ? <App /> : null}
-    </div>
-  )
+	const [ mountApp, setMountApp ] = React.useState(true);
+	return (
+		<div>
+			<label>
+				<input type="checkbox" checked={mountApp} onChange={(e) => setMountApp(e.target.checked)} /> Mount
+				Component
+			</label>
+			<hr />
+			{mountApp ? <App /> : null}
+		</div>
+	);
 }
 
-export default AppWithUnmountCheckbox
+export default AppWithUnmountCheckbox;
